433a8d22f5dd1dc154e2445dd6b8efac0ae18a42
Benson John <benson4u2c@gmail.com>
2021-06-24 16:47:12 +0400

    test

---
+++ Dev/Csharp/helloworld.cs
@@ -0,0 +1,13 @@
+// c# Hello World program
+
+using system;
+
+namespace HelloWorld
+{
+    class hello{
+        static void Main (string[] args)
+        {
+            console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Dev/python/helloworld.py
@@ -0,0 +1,3 @@
+# This program print Hello, World
+
+print('Hello,world')
